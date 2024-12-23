76843a2abd9051e0122232a9e7623578bdb4024f
Kannadasan <Kannadasan-k.ramda@aia.com>
2024-12-23 23:11:41 +0530

    FreelancerApi Project

---
+++ WebAPI/FreelancerPortal/FreelancerPortal/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FreelancerPortal.DTO;
+using FreelancerPortal.Models;
+
+
+namespace FreelancerPortal
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+                CreateMap<UserDto, User>();
+                CreateMap<SkillsetDto, Skillset>();
+                CreateMap<HobbyDto, Hobby>();
+                
+        }
+    }
+}
